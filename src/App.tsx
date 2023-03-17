55261546cfda28a41b51bd47766f6edd9351a14e
Nimer Shadida Johansson <nimer.shadida.johansson@gmail.com>
2023-03-17 17:46:36 +0100

    added routes and placeholders for creating, finding and editing notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useState } from "react";
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <Routes>
+  return (
+  <Routes>
     <Route path="/" element={<h1>Hi</h1>}/>
-  </Routes>;
+    <Route path="/new" element={<h1>New</h1>}/>
+    <Route path="/:id">
+      <Route index element={ <h1>Show</h1> }></Route>
+      <Route path="edit" element={ <h1>Edit</h1>}></Route>
+    </Route>
+    <Route path="*" element={<Navigate to={"/"} />}/>
+  </Routes>
+  )
 }
 
 export default App;
