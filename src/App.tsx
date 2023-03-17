7324ec549271d45c82e83a27491f43161d1c982c
Nimer Shadida Johansson <nimer.shadida.johansson@gmail.com>
2023-03-17 17:20:44 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return <Routes>
+    <Route path="/" element={<h1>Hi</h1>}/>
+  </Routes>;
+}
+
+export default App;
